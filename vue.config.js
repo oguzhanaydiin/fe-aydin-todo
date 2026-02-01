02990351f44b02279469b4e4cced20439a0710e2
oguzhanaydiin <oguuzaydinn@gmail.com>
2026-02-01 20:23:58 +0100

    changes on gh-pages (#14)

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/fe-aydin-todo/'
-    : '/',
+  publicPath: process.env.VUE_APP_ENVIRONMENT === 'gh-pages'
+    ? '/fe-aydin-todo/' : '/',
 };
